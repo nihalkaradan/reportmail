e445993245bcf0507ddf9d83d816b65f7d19ae64
nihalkaradan <nihalkaradan@gmail.com>
2019-11-03 21:53:54 +0530

    Update index.js
    callback function

--- shopify-express-application/index.js
+++ shopify-express-application/index.js
@@ -12,10 +12,9 @@ const apiSecret = process.env.SHOPIFY_API_SECRET;
 const scopes = 'read_products';
 const forwardingAddress = "https://2ea38a5c.ngrok.io"; // Replace this with your HTTPS Forwarding address
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+
 //Routes to installation if query parameter has shop 
+
 app.get('/shopify', (req, res) => {
   const shop = req.query.shop;
   if (shop) {
@@ -31,6 +30,27 @@ app.get('/shopify', (req, res) => {
     res.redirect(installUrl);
   } else {
     return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+  }
+});
+
+//after app is installed this endpoint gets hit 
+app.get('/shopify/callback', (req, res) => {
+  const { shop, hmac, code, state } = req.query;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
+
+  if (state !== stateCookie) {
+    return res.status(403).send('Request origin cannot be verified');
+  }
+
+  if (shop && hmac && code) {
+    res.status(200).send('Callback route');
+
+    // TODO
+    // Validate request is from Shopify
+    // Exchange temporary code for a permanent access token
+      // Use access token to make API call to 'shop' endpoint
+  } else {
+    res.status(400).send('Required parameters missing');
   }
 });
 
